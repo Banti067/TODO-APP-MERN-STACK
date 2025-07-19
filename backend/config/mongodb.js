56f431fedd51fdd323754e49b15b6b25d820b841
P Banti <pbanti8249@gmail.com>
2025-07-19 23:20:27 +0530

    some changes added

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_DB_URL, {
+    await mongoose.connect(process.env.MONGO_DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error("❌ MongoDB Connection Error:", error.message);
-    process.exit(1); // Exit process if MongoDB connection fails
+    console.log(`MongoDB connected`);
+  } catch (err) {
+    console.error("MongoDB connection failed:", err.message);
+    process.exit(1);
   }
 };
 
