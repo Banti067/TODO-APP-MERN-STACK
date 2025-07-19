4c565c7c22fe2bd90595a6f07275246187350101
P Banti <pbanti8249@gmail.com>
2025-07-19 23:40:04 +0530

    cloud developed

--- frontend/src/productServices/AuthSerives.js
+++ frontend/src/productServices/AuthSerives.js
@@ -1,14 +1,17 @@
 import axios from "axios";
 
+// Use full backend API URL
+const API = import.meta.env.MODE === "development"
+  ? "http://localhost:2025"
+  : "https://todo-app-mern-stack-prnf.onrender.com";
 
 const registerUser = (data) => {
-   console.log("Registering user with data:", data);
-  return axios.post("/api/v1/user/register", data);
+  console.log("Registering user with data:", data);
+  return axios.post(`${API}/api/v1/user/register`, data);
 };
 
-
 const loginUser = (data) => {
-  return axios.post("/api/v1/user/login", data);
+  return axios.post(`${API}/api/v1/user/login`, data);
 };
 
 const AuthServices = { registerUser, loginUser };
